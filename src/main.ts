e12e27f25e0e6e06e1c6d8cdbc6be485da0273df
Rob Deutsch <robzyb+altgithub@gmail.com>
2021-08-13 17:50:51 +1000

    listing serial ports

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,9 @@
+import SerialPort from 'serialport'
+
 console.log("Initial run");
+SerialPort.list().then((ports) => {
+    for (let idx in ports) {
+        const port = ports[idx]
+        console.log(`[${idx}]: ${port.path}`)
+    }
+})
