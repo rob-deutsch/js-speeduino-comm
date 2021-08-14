5bd5d8215aef435e38ee596d4922e47db915a4c8
Rob Deutsch <robzyb+altgithub@gmail.com>
2021-08-14 16:33:20 +1000

    Wrote hex transformer

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,21 @@
 import SerialPort from 'serialport'
 import prompt from 'prompt';
-import { finished } from 'stream'
+import { Transform, TransformCallback } from 'stream'
+import { createBrotliCompress } from 'zlib';
+
+class HexTransformer extends Transform {
+    _transform(chunk: any, encoding: BufferEncoding, cb: TransformCallback): void {
+        let cursor = 0
+        while (cursor < chunk.length) {
+            this.push("0x" + chunk[cursor].toString(16) + " ")
+            cursor++
+        }
+        cb();
+    }
+    _flush(cb: TransformCallback) {
+        cb()
+    }
+}
 
 console.log("Initial run");
 SerialPort.list().then(async (ports) => {
@@ -12,10 +27,10 @@ SerialPort.list().then(async (ports) => {
     const port = ports[parseInt(choice['id'] as string)]
 
     const sp = new SerialPort(port.path, {baudRate: 115200, autoOpen: false})
-    const outputPipe  = sp.pipe(process.stdout)
+    sp.pipe(new HexTransformer()).pipe(process.stdout)
     sp.open((err) => {
         if (err) throw err
         sp.write('S')
-        sp.write(Buffer.from([0x72, 0x00, 0x30, 0x00, 0x00, 0x72, 0x00]))
+        // sp.write(Buffer.from([0x72, 0x00, 0x30, 0x00, 0x00, 0x72, 0x00]))
     })
 })
