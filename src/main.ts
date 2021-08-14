c9ceaa4614f308342d488099a3f280e7c3670edf
Rob Deutsch <robzyb+altgithub@gmail.com>
2021-08-14 16:55:07 +1000

    Abstracted out serial port

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,26 @@ class HexTransformer extends Transform {
     }
 }
 
+class SpeeduinoComm {
+    path: string
+    sp: SerialPort
+
+    constructor(path: string) {
+        this.path = path
+        this.sp = new SerialPort(path, {baudRate: 115200, autoOpen: false})
+        this.sp.pipe(new HexTransformer()).pipe(process.stdout)
+    }
+
+    open(cb?: (error?: Error | null) => void) {
+        this.sp.open(cb)
+    }
+
+    write(buffer: string | number[] | Buffer) {
+        this.sp.write(buffer)
+    }
+}
+
+
 console.log("Initial run");
 SerialPort.list().then(async (ports) => {
     for (let idx in ports) {
@@ -26,11 +46,10 @@ SerialPort.list().then(async (ports) => {
     const choice = await prompt.get(['id'])
     const port = ports[parseInt(choice['id'] as string)]
 
-    const sp = new SerialPort(port.path, {baudRate: 115200, autoOpen: false})
-    sp.pipe(new HexTransformer()).pipe(process.stdout)
-    sp.open((err) => {
+    const speedy = new SpeeduinoComm(port.path)
+    speedy.open((err) => {
         if (err) throw err
-        sp.write('S')
-        // sp.write(Buffer.from([0x72, 0x00, 0x30, 0x00, 0x00, 0x72, 0x00]))
+        speedy.write('S')
+        speedy.write(Buffer.from([0x72, 0x00, 0x30, 0x00, 0x00, 0x72, 0x00]))
     })
 })
