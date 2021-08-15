f4aa404d443c39945bc681239845cbfb870802a6
Rob Deutsch <robzyb+altgithub@gmail.com>
2021-08-15 13:53:29 +1000

    Temporary checkpoint

--- src/main.ts
+++ src/main.ts
@@ -17,6 +17,67 @@ class HexTransformer extends Transform {
     }
 }
 
+class SpeeduinoParser extends Transform {
+    rp: SpeeduinoResponseParser | null
+    constructor() {
+        super()
+        this.rp = null
+    }
+    addParser(rp: SpeeduinoResponseParser) {
+        this.rp = rp
+    }
+    _transform(chunk: any, encoding: BufferEncoding, cb: TransformCallback): void {
+        if (this.rp) {
+            let remaining = this.rp.write(Buffer.from(chunk))
+            if (remaining) {
+                if (remaining.length !== 0) {
+                    // There are extra bytes need to give to next parser?
+                }
+            }
+            return
+        }
+        // TODO: This is a very big error state
+    }
+    _flush(cb: TransformCallback) {
+        
+    }
+}
+interface SpeeduinoResponseParser {
+    write(chunk: Buffer): Buffer | null
+    stop(): void
+}
+
+class SResponse {
+    length: number
+    position: number
+    buffer: Buffer
+    cb?: (response: Buffer) => void
+    constructor(cb?: (response: Buffer) => void) {
+        this.length = 21
+        this.position = 0
+        this.buffer = Buffer.alloc(this.length)
+        this.cb = cb
+    }
+    write(chunk: Buffer): Buffer | null {
+        let cursor = 0
+        while (cursor < chunk.length) {
+            this.buffer[this.position] = chunk[cursor]
+            cursor++
+            this.position++
+            if (this.position === this.length) {
+                if (this.cb) {
+                    this.cb(this.buffer)
+                }
+                return chunk.slice(this.position, chunk.length)
+            }
+        }
+        return null
+    }
+    stop() {
+
+    }
+}
+
 class SpeeduinoComm {
     path: string
     sp: SerialPort
@@ -34,6 +95,12 @@ class SpeeduinoComm {
     write(buffer: string | number[] | Buffer) {
         this.sp.write(buffer)
     }
+
+    getVersion(): Promise<Buffer> {
+        return new Promise((resolve, reject) => {
+            resolve(Buffer.alloc(0))
+        })
+    }
 }
 
 
@@ -49,7 +116,10 @@ SerialPort.list().then(async (ports) => {
     const speedy = new SpeeduinoComm(port.path)
     speedy.open((err) => {
         if (err) throw err
-        speedy.write('S')
-        speedy.write(Buffer.from([0x72, 0x00, 0x30, 0x00, 0x00, 0x72, 0x00]))
+        // speedy.write('S')
+        speedy.getVersion().then((response) => {
+            console.log(response)
+        })
+        // speedy.write(Buffer.from([0x72, 0x00, 0x30, 0x00, 0x00, 0x72, 0x00]))
     })
 })
