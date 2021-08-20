576b07928d3ccb7e29d402ce3fae81bb782d73d3
Rob Deutsch <robzyb+altgithub@gmail.com>
2021-08-20 16:02:03 +1000

    Better error handling of serial port

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,44 @@ import prompt from 'prompt';
 import { HalfDuplexPackets } from './HalfDuplexPackets'
 import { Speeduino } from './Speeduino'
 
+async function logCPS(port: SerialPort.PortInfo, interval: number) {
+    // Setup the required connections
+    const sp = new SerialPort(port.path, { baudRate: 115200, autoOpen: false })
+    sp.on('error', () => console.log("Serial port error"))
+    const conn = new HalfDuplexPackets(sp)
+    conn.on('unexpected', (data) => { console.log("Unexpected data:", data); throw "ERROR" })
+    const speedy = new Speeduino(conn)
+
+    // Make sure we can connect
+    try {
+        await new Promise<void>((resolve, reject) => sp.open((err) => err ? reject(err) : resolve()))
+    } catch (e) {
+        console.log("Couldn't connect:", e)
+        return
+    }
+
+    // Display the signatures
+    speedy.signature().then((response) => {
+        console.log("Signature:", response)
+    })
+    speedy.versionInfo().then((response) => {
+        console.log("Version info:", response)
+    })
+
+    while (true) {
+        const everyOneSecond = new Promise<void>(resolve => setTimeout(resolve, interval))
+        try {
+            const response = await speedy.raw.outputChannels(121)
+            console.log((new Date).toISOString(), "Cycles per second:", (response[26] << 8) + response[25])
+        } catch (error) {
+            console.log("Error on outputChannels:", error.message)
+            sp.close()
+            return
+        }
+        await everyOneSecond
+    }
+}
+
 SerialPort.list().then(async (ports) => {
     for (let idx in ports) {
         const port = ports[idx]
@@ -11,28 +49,11 @@ SerialPort.list().then(async (ports) => {
     const choice = await prompt.get(['id'])
     const port = ports[parseInt(choice['id'] as string)]
 
-    const sp = new SerialPort(port.path, { baudRate: 115200, autoOpen: false })
-    const conn = new HalfDuplexPackets(sp)
-    conn.on('unexpected', (data) => {console.log("Unexpected data:", data); throw "ERROR"})
-    const speedy = new Speeduino(conn)
-
-    // speedy.pipe(new HexTransformer()).pipe(process.stdout)
-    sp.open((err) => {
-        if (err) throw err
-        speedy.signature().then((response) => {
-            console.log("Signature:", response)
-        })
-        speedy.versionInfo().then((response) => {
-            console.log("Version info:", response)
-        })
-
-        let logCPS = () => {
-            speedy.raw.outputChannels(121).then((response) => {
-                console.log((new Date).toISOString(), "Cycles per second:", (response[26]<<8) + response[25])
-            }).catch((error) => {
-                console.log("Error on outputChannels:", error.message)
-            })
+    const continuallLogCPS = async (interval: number) => {
+        while (true) {
+            await logCPS(port, interval)
+            await new Promise<void>(resolve => setTimeout(resolve, interval))
         }
-        setInterval(logCPS, 1000)
-    })
+    }
+    continuallLogCPS(1000)
 })
